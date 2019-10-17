d676530d9a1bf41d9d5814f14faf54514da11b93
Harry Martin <up896106@myport.ac.uk>
2019-10-17 17:08:18 +0100

    Changed Stack from abstract class to interface

--- Stack.java
+++ Stack.java
@@ -1,9 +1,9 @@
 package dataStructures;
 
-public abstract class Stack<T>{
-	public abstract void Push(T e) throws Exception;
-	public abstract T Pop() throws Exception;
-	public abstract T Peek() throws Exception;
-	public abstract boolean Empty();
-	public abstract boolean Full();
+public interface Stack<T>{
+	public void Push(T e) throws Exception;
+	public T Pop() throws Exception;
+	public T Peek() throws Exception;
+	public boolean Empty();
+	public boolean Full();
 }
